3fe4b3b141d9d4541f5c7eb4379373fe92bca2ba
romakvadrat <romakvadrat@gmail.com>
2020-03-04 18:13:28 +0000

    js-01

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// alert('Hello!');
+
+let greetings = 'World!';
+// alert(greetings);
+
+const message = 'Hello ' + greetings;
+alert(message);
+
+const userAge = parseFloat(prompt('Enter your age'));
+alert('Your age is: ' + userAge);
+
+// Зробити нову змінну nextAge
+// Присвоїти в неї userAge збільшений на один
+// Вивести на екран nextAge
+
+const nextAge = add(userAge, 1);
+alert(nextAge);
+
+if (userAge >= 18) { // > < >= <= === == !== != && ||
+    alert('You are adult!');
+} else {
+    alert('You are underaged');
+}
+
+function add(a, b) {
+    const result = a + b;
+    return result;
+}
